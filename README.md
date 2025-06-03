43c57f8ba61ddd6666c9c7573317f2e8bfcca8b3
Diana-Bazdyreva <cruzerblade19912@gmail.com>
2025-06-03 16:49:02 +0300

    final commit

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
-# Final_project
+# Документация проекта "Final_project"
 
-## Автоматизации тестирования на python
+## Автоматизация тестирования на python
 
 ### Стек:
-- pytest
-- selenium
-- requests
-- 
+- pytest (Основной язык программирования для написания тестов)
+- selenium (Библиотека для автоматизации взаимодействия с веб-браузером)
+- requests (Фреймворк для написания и запуска тестов)
+- allure (Инструмент для генерации отчетов о выполнении тестов)
 
+### Форматирование кода
+
+- Код форматируется в соответствии с PEP 8 (стиль написания кода на Python).
+- Все шаги теста размечаются с помощью `@allure.step` или `with allure.step` для улучшения читаемости отчетов.
 
 ### Шаги
 1. Склонировать проект 'git clone https://github.com/Diana-Bazdyreva/Final-project.git
 2. Установить зависимости
 3. Запустить тесты 'pytest'
+4. Сгенерировать отчет 'allure generate allure-files -o allure-report'
+5. Открыть отчет 'allure open allure-report'
 
 
 ### Библиотеки (!)
 - pyp install pytest
 - pip install selenium
+- pip3 install allure-pytest
+
+### Ссылка на финальный проект: 
+- тест-план финального проекта: https://beshtau.yonote.ru/share/e12a6d60-938e-42f7-a4b2-635a114b8902
+- отчет о тестировании финального проекта: https://beshtau.yonote.ru/share/2ff41495-7e2b-4601-9b58-46c96641cd99
--- test_api/test_api.py
+++ test_api/test_api.py
@@ -1,22 +1,105 @@
 import requests
+import allure
+from config import AUTH_TOKEN, base_url
 
 
-AUTH_TOKEN = "24K82W5-7PWM7CF-N0MFYK5-1VVXXAW"
-base_url = "https://api.kinopoisk.dev"
+@allure.feature("API Кинопоиск")
+@allure.story("Поиск фильма по названию")
+def test_movie_title_search():
+    """
+    Проверяет работоспособность API-поиска фильма по названию.
+    Ожидаемый результат: Возвращается список фильмов,
+    соответствующих указанному названию
+    """
+    with allure.step("GET-запрос на поиск фильма по названию"):
+        params = {"query": "работа без авторства"}
+        headers = {"X-KP-API-KEY": AUTH_TOKEN}
+        base_url + "/v1.4/movie/search"
+        response = requests.get(base_url, params=params, headers=headers)
+    with allure.step("Проверка статус-кода на соответствие ожидаемому"):
+        assert response.status_code == 200
 
-def test_movie_search():
-    """Get запрос 'Поиск фильма по названию'"""
-    params = {
-        'query': 'работа без авторства'
-    }
-    headers = {
-        'X-KP-API-KEY': AUTH_TOKEN
-    }   
-    base_url + '/v1.4/movie/search'
-    response = requests.get(base_url, params=params, headers=headers)
-            # Проверяем статус-код ответа
-    assert response.status_code == 200, f'Ошибка: неверный статус-код ({response.status_code})'
-        
-    
-        
-        
+
+@allure.feature("API Кинопоиск")
+@allure.story("Получение списка жанров фильмов")
+def test_getting_list_genre():
+    """
+    Проверка возможности получения списка жанров фильмов.
+      Ожидаемый результат: Возвращение полного списока жанров
+    """
+    with allure.step("GET-запрос на получение списка жанров"):
+        params = {"field": "genres.name"}
+        headers = {"X-KP-API-KEY": AUTH_TOKEN}
+        base_url + "/v1/movie/possible-values-by-field"
+        response = requests.get(base_url, params=params, headers=headers)
+
+    with allure.step("Проверка статус-кода на соответствие ожидаемому"):
+        assert response.status_code == 200
+
+
+@allure.feature("API Кинопоиск")
+@allure.story("Получение информации о фильме по ID")
+def test_movie_id_search(id="435"):
+    """
+    Проверка возможности получения детальной информации
+    о фильме по ID. Ожидаемый результат: Возвращение информации
+    о конкретном фильме
+    """
+    with allure.step("GET-запрос на получение информации о фильме по ID"):
+        headers = {"X-API-KEY": AUTH_TOKEN}
+    response = requests.get(f"{base_url}/v1.4/movie/{id}", headers=headers)
+
+    with allure.step("Проверка статус-кода на соответствие ожидаемому"):
+        assert response.status_code == 200
+
+
+@allure.feature("API Кинопоиск")
+@allure.story(
+    "Негативная проверка на запрос "
+    "с невалидным параметром: год выпуска фильма"
+)
+def test_invalid_year_release():
+    """
+    Проверка обработки запроса с невалидным
+    годом выпуска фильма. Ожидаемый результат: статус-код 400
+    """
+    with allure.step("GET-запрос с невалидным годом выпуска фильма"):
+        params = {
+            "page": 1,
+            "limit": 10,
+            "year": "1850",
+        }
+
+    headers = {"X-API-KEY": AUTH_TOKEN}
+
+    response = requests.get(
+        base_url + "/v1.4/movie", params=params, headers=headers)
+    with allure.step("Проверка соответствия статус-кода ожидаемому"):
+        assert response.status_code == 400
+
+
+@allure.feature("API Кинопоиск")
+@allure.story(
+    "Негативная проверка обработки "
+    "запроса с невалидным параметром: страна производства фильма"
+)
+def test_invalid_country():
+    """
+    Проверка обработки запроса с указанием несуществующей страны.
+    Ожидаемый результат: Статус-код: 200, длина списка фильмов: 0
+    """
+    with allure.step(
+        "GET-запрос с невалидным параметром:" " страна производства фильма"
+    ):
+        params = {"page": 1, "limit": 10, "countries.name": "Зурумбия"}
+    headers = {"X-API-KEY": AUTH_TOKEN}
+    endpoint = base_url + "/v1.4/movie"
+    response = requests.get(endpoint, params=params, headers=headers)
+
+    result = response.json()
+
+    with allure.step("Проверка статус-кода на соответствие ожидаемому"):
+        assert response.status_code == 200
+
+    with allure.step("Проверка длины списка фильмов"):
+        assert len(result.get("docs", [])) == 0
